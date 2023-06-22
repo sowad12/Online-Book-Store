67bd1135b1e187f7ff2eec224eba9f241b7ccae7
Sowad Islam <sowad@wmglobal.local>
2023-06-22 20:04:53 +1000

    route fix

--- BookShop.Mvc/BookShop.Mvc/Program.cs
+++ BookShop.Mvc/BookShop.Mvc/Program.cs
@@ -43,6 +43,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
+
+	);
 
 app.Run();
